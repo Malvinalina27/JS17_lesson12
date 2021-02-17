d81e9247dddab0c79e322fbd7fc04650cc2cedab
Malvinalina27 <rasberry-07@mail.ru>
2021-02-17 17:02:59 +0300

    Добавление в localStorage, запрет отпраки пустого дела, удаление дела

--- js/script.js
+++ js/script.js
@@ -5,25 +5,28 @@ const todoControl = document.querySelector('.todo-control'), //форма
       todoList  = document.querySelector('.todo-list'),//сюда добавляем дела
       todoCompleted = document.querySelector('.todo-completed');//выполненные дела
 
-const todoData = [];
+let todoData = [];
 
 //добавление дел на страницу(рендерит)
 const render = function() { 
   todoList.textContent = '';
   todoCompleted.textContent = '';
 
-  todoData.forEach(function(item){
+  todoData.forEach(function(item, i){
     const li = document.createElement('li');
   
     li.classList.add('todo-item');
    
     li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
-      '<div class="todo-buttons">' + 
+      '<div class="todo-buttons">' +
         '<button class="todo-remove"></button>' +
         '<button class="todo-complete"></button>' +
       '</div>';
     
-    if (item.completed) {
+    
+
+    //выполненные и невыполненные дела
+    if (item.completed) { 
       todoCompleted.append(li);
     } else {
       todoList.append(li);
@@ -32,30 +35,45 @@ const render = function() {
     const btnTodoComplete = li.querySelector('.todo-complete');
     btnTodoComplete.addEventListener('click', function() {
       item.completed = !item.completed;
+      localStorage.setItem('todo', JSON.stringify(todoData));
       render();
     });
 
     //удаление дела
     const btnTodoRemove = li.querySelector('.todo-remove');
-    btnTodoRemove.addEventListener('click', function() {    
-      li.remove(); 
+    btnTodoRemove.addEventListener('click', function() { 
+      todoData.splice(i, 1);
+      localStorage.setItem('todo', JSON.stringify(todoData));
+      render();
     });
-
+    
 
   });
+ 
 };
 
+if (localStorage.getItem('todo')) {
+  todoData = JSON.parse(localStorage.getItem('todo'));
+  render();
+} 
+
 todoControl.addEventListener('submit', function(event){
-  event.preventDefault(); //не перезагружается страница при нажатиии на +
-  //происходит событие submit
-  const newToDo = { //новый объект
+  event.preventDefault(); 
+  
+  const newToDo = { 
     value: headerInput.value,
     completed: false
   };
+
+  if (headerInput.value !== '' && headerInput.value.trim() && headerInput.value !== null) {
+    todoData.push(newToDo);
+  }
+
+  headerInput.value = '';  
   
-  todoData.push(newToDo);
-  //вызываем рендер для обновления списка дел
+  localStorage.setItem('todo', JSON.stringify(todoData));
   render();
 });
+
 
 render();
